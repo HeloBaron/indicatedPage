36a1d85fd6ada20ca95440794ba40795cdb2bbe3
HeloBaron <helo.athayde@gmail.com>
2025-02-28 14:40:10 -0300

    ..

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import logo from '../../assets/logo.svg'
+
 import { InviteLinkInput } from './inviteLinkInput'
 import { Ranking } from './ranking'
 import { Stats } from './stats'
@@ -10,7 +10,6 @@ export default function InvitePage() {
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
-        <Image src={logo} alt="devstage" width={108.5} height={30} />
         <div className="space-y-2">
           <h1 className="text-4xl font-semibold font-heading text-gray-100 leading-none">
             Inscrição confirmada!
